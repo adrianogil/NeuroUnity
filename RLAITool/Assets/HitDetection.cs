12cdb96e49c9e4c8c374381d0d71c07436e5138a
Thiago Figueira <tsantosfigueira@outlook.com>
2017-07-09 12:56:18 -0400

    Added tic-tac-toe
    Also added a performance indicator to measure how well the car performs
    while controlled by the AI.

---
+++ RLAITool/Assets/HitDetection.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HitDetection : MonoBehaviour
+{
+    public Text timeCounter;
+    private float timer = 0;
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Wall")
+        {
+            Time.timeScale = 0;
+            Debug.Log("Wall hit");
+        }
+    }
+
+    private void Update()
+    {
+        timer += Time.deltaTime;
+        timeCounter.text = timer.ToString();
+    }
+}
